12ddd898bd17e6172cfa883141a19dd37b16cea1
Martín Ramallo <miramallo@gmail.com>
2023-07-23 19:30:23 -0300

    create the style of the components

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>¿Quien es este pokemon?</h1>
+    <PokemonPicture />
+    <PokemonOptions />
+
+</template>
+
+<script>
+
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+}
+</script>
+
+<style>
+
+</style>
